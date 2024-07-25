7d8d9698bba55e9eb3a62b6b1814dedb384e3278
Benjamin Torfu <btorfu@proton.me>
2024-07-25 05:12:39 +0000

    prom

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,9 @@
-function getResponseFromAPI() {
-	return new Promise((settled, rejected) => {
-		setTimeout(() => {
-			const success = true;
+function getResponseFromAPI () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
 
-			if (success)
-				settled("API responded");
-			else
-				rejected("API failed");
-		}, 1000);
-	});
+      if (success) { resolve('API responded'); } else { reject(new Error('API failed')); }
+    }, 1000);
+  });
 }
