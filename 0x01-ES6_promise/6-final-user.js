a6d82e2cc88f8137c3c621b0aa737a581a8b5778
Benjamin Torfu <btorfu@proton.me>
2024-07-26 16:22:28 +0000

    review

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,24 +1,21 @@
-import { signUpUser } from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export function handleProfileSignup (firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => {
-      return results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: result.value
-          };
-        } else {
-          return {
-            status: 'rejected',
-            value: result.reason
-          };
-        }
-      });
-    });
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
+    }));
 }
