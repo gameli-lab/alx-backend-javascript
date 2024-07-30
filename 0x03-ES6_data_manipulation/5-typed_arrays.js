0e13b418d2db66870dba1360db621d82b54b18f0
Benjamin Torfu <btorfu@proton.me>
2024-07-30 12:40:46 +0000

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,12 @@
 const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+
   if (position < 0 || position >= length) {
     throw new Error('Position outside the array boundary');
   }
-  const newArray = new Int8Array(length);
-  newArray[position] = value;
-  return newArray;
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+  return dataView;
 };
 
 export default createInt8TypedArray;
